5ae7677702aab08fb904f6729377c0f7731c7c20
namankrbhagat <namanbhagat.8161@gmail.com>
2026-04-18 01:17:16 +0530

    fixed token bugs

--- backend/config/token.js
+++ backend/config/token.js
@@ -5,10 +5,12 @@ export const generateToken = async (userId,res) => {
     expiresIn: "7d"
   })
 
+  const isProd = process.env.NODE_ENV === "production";
+  
   res.cookie("token", token, {
-    secure:true,
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000, //MS
-    http: true // prevent XSS attacks cross-site scripting attacks
+    httpOnly: true, // fix typo (was http: true)
+    secure: isProd, // Must be true for SameSite='none'
+    sameSite: isProd ? "none" : "strict", // 'none' is exactly required for cross-domain Vercel to Render communication
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   })
 }
