fb09f1eb34b64aaeb93e0b2452d8a096f8960c57
namankrbhagat <namanbhagat.8161@gmail.com>
2026-04-18 01:00:18 +0530

    cors link update

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ configDotenv()
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: process.env.CLIENT_URL || [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173"
+  ],
   credentials:true
 }))
 
